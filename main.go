9f6594cec3fe9c4cae8f49ef820f75eaec937508
moti <motinyba@gmail.com>
2022-02-19 21:00:30 +0200

    first try with go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+const NUMBER = 10
+
+func main() {
+	var aString string = "moti"
+	fmt.Println(aString)
+	fmt.Println("Hello world, this is GO!")
+	anotherString := "this is the string"
+	fmt.Println(anotherString)
+	fmt.Println(NUMBER)
+}
